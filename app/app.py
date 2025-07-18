81783042e6f1d4db40bc89031da896eaa91b4d27
Alexandre Bidet <alexandre.bidet44@gmail.com>
2025-07-18 15:58:17 +0200

    fix: remove persistent data code cause didn't make it work

--- app/app.py
+++ app/app.py
@@ -1,32 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/servers.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
-class Server(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    ip = db.Column(db.String(80), nullable=False)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# In-memory storage for servers
+servers = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         name = request.form['name']
         ip = request.form['ip']
-        server = Server(name=name, ip=ip)
-        db.session.add(server)
-        db.session.commit()
+        server = {'name': name, 'ip': ip}
+        servers.append(server)
         return redirect(url_for('index'))
-    servers = Server.query.all()
     return render_template('index.html', servers=servers)
 
 if __name__ == '__main__':
